8cabf78a19b8b9f71d778118ac30b6ab2b52c5ca
Arvinddurai <83328108+Arvinddurai@users.noreply.github.com>
2021-07-22 09:24:23 +0530

    Update exeter-coding-challenge.py

--- exeter-coding-challenge.py
+++ exeter-coding-challenge.py
@@ -1,28 +1,55 @@
-import re
-import csv
-import time
-start = time.time()
-mydict={}
-with open('C:/exeter/TranslateWords Challenge/french_dictionary.csv', mode='r') as fr_dict:
-    reader = csv.reader(fr_dict)
-    mydict = {rows[0]:rows[1] for rows in reader}
-f=open("C:/exeter/TranslateWords Challenge/find_words.txt","r")
-# content=''
-# arr=[]
-# with open("C:/exeter/TranslateWords Challenge/t8.shakespeare.txt","r+")as p:
-#     for line in p:
-#         arr.append(line)
-#     content=''.join(arr)
+import csv, sys, time
+import os
+import psutil
 
-p= open("C:/exeter/TranslateWords Challenge/t8.shakespeare.txt","r")
-content=p.read()
-para = re.split("\s|(?<!\d)[,.](?!\d)",content)
-#print(f.read())
-find = set(words for line in f for words in line.strip('\n\t').split(" "))
-for words in find:
-    if words in para:
-        if words in mydict:
-            content=content.replace(words,str(mydict[words])) 
-print(content)          
-end = time.time()
-print("Execution time in seconds: ",(end - start))
+def initialize_french_dictionary():
+    with open('C:/exeter/TranslateWords Challenge/french_dictionary.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            french_dict[row[0]] = [row[1],0]
+
+def translate(input_file):
+    fp1 = open('t8.shakespeare.translate.txt', 'w')
+    check_list = french_dict.keys()
+    with open(input_file, 'r') as input_text:
+        while True:
+            line = input_text.readline()
+            if not line:
+                break
+            buffer = ''
+            for word in line.split():
+                filtered = filter(str.isalpha,word)
+                query = "".join(filtered)
+                if query in check_list:
+                    query_success = french_dict[query][0]
+                    french_dict[query][1] += 1
+                    word = word.replace(query, query_success)
+                buffer += word + ' '
+            buffer = buffer.strip()
+            fp1.write(buffer + '\n')
+    fp1.close()
+    print("File Translated")
+    return True
+
+
+def generate_performance(process_time, memory_info):
+    with open('performance.txt', 'w') as file:
+        file.write(f"Time to process : {process_time} seconds\n") 
+        file.write(f"Memory used : {memory_info} MB")
+    
+
+if __name__ == '__main__':
+    process_start_time = time.time()
+    french_dict = {}
+    input_file = 'C:/exeter/TranslateWords Challenge/t8.shakespeare.txt'
+
+    if len(sys.argv) == 2:
+        input_file = sys.argv[1]
+
+    initialize_french_dictionary()
+    translate(input_file)
+    process_complete_time = time.time()
+    memory_used = psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2
+    process_time = process_complete_time - process_start_time
+
+    generate_performance(process_time, memory_used)
